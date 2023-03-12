2e02e79f3db6acef72559ad9089811fcf694227b
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2023-03-12 12:01:54 -0700

    first

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
+      <body className="bg-white">
         <Main />
         <NextScript />
       </body>
